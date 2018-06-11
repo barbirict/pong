5e177f22f3a2d0fc168702a8da9a8d15d680613b
WuTanB <40025611+WuTanB@users.noreply.github.com>
2018-06-11 22:24:29 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,69 +16,166 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include "session.h"
+#include <mutex>
+#include <chrono>
 using namespace std;
 #define PORT 28960
 
 
 bool isEnd=false;
 
-game* g;
-int server_fd, new_socket, valread;
+game* g;//=new game();
+session* sesh;//=new session(g);
+int server_fd, new_socket,sock, valread;
 
 int opt =1;
-char buff[100]={};
-
-
-int _kbhit()
+int buff[11]={};
+mutex game_mutex;
+mutex g2;
+void emptyBuff(){
+  for(int i=0; i<11; i++){
+    buff[i]=NULL;
+  }
+}
+int _kbhit(void)
 {
+    int ch = getch();
 
-    static const int STDIN = 0;
-
-    static bool initialized = false;
-
-
-
-    if (! initialized)
-    {
-
-        // Use termios to turn off line buffering
-
-        termios term;
-
-        tcgetattr(STDIN, &term);
-
-        term.c_lflag &= ~ICANON;
-
-        tcsetattr(STDIN, TCSANOW, &term);
-
-        setbuf(stdin, NULL);
-
-        initialized = true;
-
+    if (ch != ERR) {
+        ungetch(ch);
+        return true;
+    } else {
+        return false;
     }
-
-    int bytesWaiting;
-
-    ioctl(STDIN, FIONREAD, &bytesWaiting);
-
-    return bytesWaiting;
-
 }
 
+void t_move1(){
+  char ch;
+  while(isEnd!=true)
+
+  {
+
+      //g->print_game();
+
+      if(_kbhit())
+      {
+        game_mutex.lock();
+          ch=getch();
+
+
+
+          if(ch=='w')
+          {
+
+              g->movePlayer1Up();
+              //g->print_game();
+
+          }
+          else if(ch=='s')
+
+          {
+
+              g->movePlayer1Down();
+              //g->print_game();
+
+          }
+          game_mutex.unlock();
+          this_thread::sleep_for (std::chrono::milliseconds(1));
+      }
+
+  }
+}
+void t_move2(){
+  char ch;
+  while(isEnd!=true)
+
+  {
+
+    //g->print_game();
+
+      if(_kbhit())
+      {
+          game_mutex.lock();
+          ch=getch();
+
+          if(ch=='8')
+          {
+
+              g->movePlayer2Up();
+
+          }
+          else if(ch=='5')
+
+          {
+
+              g->movePlayer2Down();
+
+          }
+          game_mutex.unlock();
+
+      }
+
+      this_thread::sleep_for (std::chrono::milliseconds(1));
+  }
+}
+//session controller
+
+  void t_inet_send(){
+    while(isEnd!=true){
+    game_mutex.lock();
+    sesh->setCurrent(g);
+    sesh->preparePacketServer(buff);
+    send(new_socket , &buff , sizeof(buff),MSG_CONFIRM);
+    emptyBuff();
+    valread=read(new_socket,&buff , sizeof(buff));
+    if(valread==-1)cout<<"NAPAKA VALREAD"<<endl;
+    sesh->setCurrent(g);
+    sesh->openPacketServer(buff);
+    g=sesh->getCurrent();
+    emptyBuff();
+    game_mutex.unlock();
+    this_thread::sleep_for (std::chrono::milliseconds(1));
+
+  }
+}
+
+  void t_inet_send_client(){
+   while(isEnd!=true){
+    game_mutex.lock();
+    emptyBuff();
+    sesh->setCurrent(g);
+    //cout<<"vreu"<<endl;
+    sesh->preparePacketClient(buff);
+    //cout<<"vreu"<<endl;
+    if(send(sock , &buff , sizeof(buff),MSG_CONFIRM) == -1){
+      perror("napaka!");
+      exit(-3500);
+    }
+    valread=read(sock,&buff , sizeof(buff));
+    //cout<<"v clientuA"<<endl;
+    sesh->setCurrent(g);
+    //cout<<"v clientuB"<<endl;
+    sesh->openPacketClient(buff);
+    //cout<<"v clientuC"<<endl;
+    g=sesh->getCurrent();
+    emptyBuff();
+    game_mutex.unlock();
+    this_thread::sleep_for (std::chrono::milliseconds(1));
+  }
+  }
+int x=1,y=1,z=0;
 void t_main()
 {
-
-
-
-    int x=0,y=0,z=0;
-
-    x= rand() % 1 + (-1);
-
-    y= rand() % 1 + (-1);
-
-    while(g->getP1()->getScore()<5 && g->getP2()->getScore()<5)
+  //cout<<"tmain"<<endl;
+    while(1)
     {
+      if(g->getP1()->getScore()==5 || g->getP2()->getScore()==5){
+        cout << endl << "konec igre" << endl;
+        break;
+      }
 
+      game_mutex.lock();
         if(z==1 || z==2)
         {
 
@@ -92,66 +189,49 @@ void t_main()
 
             g->spawnBall();
 
+
         }
 
         else
         {
-
             g->moveBall(x,y,z);
-
-            usleep(300000);
+            g->print_game();
+            //cout<<"moveBall"<<x<<" "<<y<<" "<<z<<endl;
+            usleep(1000000);
 
         }
-
+          game_mutex.unlock();
+          this_thread::sleep_for (std::chrono::milliseconds(1));
+      /*  thread tt(t_inet_send);
+        thread tg(t_inet_get);
+        tt.join();
+        tg.join();*/
     }
     isEnd=true;
 
 }
-
-
-void t_inet(){
-
-    while(!isEnd){
-      valread=read(new_socket,buff,1);
-       if(buff[0]=='8')
-
-      {
-
-          //cout<<"gor";
-
-          g->movePlayer2Up();
-
-      }
-
-      else if(buff[0]=='5')
-
-      {
-
-          //cout<<"dol";
-
-          g->movePlayer2Down();
-
-      }
-      g->print_game();
+void t_main_c(){
+  while(!isEnd){
+    if(g->getP1()->getScore()==5 || g->getP2()->getScore()==5){
+      cout << endl << "konec igre" << endl;
+      break;
     }
-	}
-
-  void t_inet_client(){
-
+    game_mutex.lock();
+    //cout<<"a"<<g->getZoga()->getPosX()<<endl;
+    g->print_game();
+    game_mutex.unlock();
+    this_thread::sleep_for (std::chrono::milliseconds(1));
   }
+}
+
+
+
 int main()
 
 {
 	string ip;
     clear();
-
-    //initscr();
-
-    //cbreak();
-
-   //noecho();
-	//printw("1...Server\n2...Client\n3...Local\nchoice:");
-	cout<<"YES ESE "<<endl;
+	cout<<"Server: 1"<<endl << "Client: 2" << endl;
 	char cha;
 	cin>>cha;
 	switch(cha){
@@ -159,11 +239,11 @@ int main()
     {
         cout<<"tu";
         struct sockaddr_in address;
-cout<<"tu";
+        cout<<"tu";
         int addrlen = sizeof(address);
         cout<<"tu";
 
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == 0)
         {
             perror("socket failed");
             exit(EXIT_FAILURE);
@@ -192,7 +272,7 @@ cout<<"tu";
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    cout<<"Client connected!"<<endl;
+
 
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                        (socklen_t*)&addrlen))<0)
@@ -200,14 +280,14 @@ cout<<"tu";
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    cout<<"yes";
-    thread t2(t_inet);
+    cout<<"Client connected!"<<endl;
 
 
     initscr();
     cbreak();
     noecho();
     g=new game();
+    sesh=new session(g);
 
     g->print_game();
 
@@ -217,66 +297,28 @@ cout<<"tu";
 
     g->print_game();
 
+
     thread t1(t_main);
+    thread t4(t_move1);
+    thread t2(t_inet_send);
+    //thread t3(t_inet_get);
+    //while(1) g->print_game();
 
-    char ch;
+    t1.join();
+    t2.join();
+    //t3.join();
+    t4.join();
 
-    bool running=true;
-
-    while(running)
-
-    {
-        g->print_game();
-
-        if(_kbhit())
-        {
-            ch=getch();
-
-            if(isEnd==true)
-                t1.join();
-                t2.join();
-
-            if(ch=='w')
-
-            {
-
-                cout<<"gor";
-
-                g->movePlayer1Up();
-
-            }
-
-            else if(ch=='s')
-
-            {
-
-                cout<<"dol";
-
-                g->movePlayer1Down();
-
-            }
-
-        }
-
-
-
-    }
-
-    endwin();
 			break;
 		}
     case '2':{
-            //printw("\nVnesi IP naslov streznika: ");
-            cout<<"Vnesi IP :"<<endl;
-            cin>>ip;
-            //printw("Vnesi port: ");
-            /*cout<<"Vnesi Port:"<<endl;
-            cin>>port;*/
+      //printw("\nVnesi IP naslov streznika: ");
+      cout<<"Vnesi IP :"<<endl;
+      cin>>ip;
       struct sockaddr_in address;
-      int sock = 0, valread;
       struct sockaddr_in serv_addr;
       //char *hello = "Hello from client";
-      if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+      if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
       {
         cout << "\n Socket creation error \n";
         return -1;
@@ -301,73 +343,32 @@ cout<<"tu";
         cout << "\nConnection Failed \n";
         return -1;
       }
-      /*send(sock , hello , strlen(hello) , 0 );
-      valread = read( sock , buffer, 1024);
-      printf("Hello message sent\n");
-      printf("%s\n",buffer );*/
 
       initscr();
       cbreak();
       noecho();
       g=new game();
-
-      g->print_game();
-
-      g->addPlayersToArena();
-
       g->spawnBall();
-
+      g->addPlayersToArena();
       g->print_game();
+      sesh=new session(g);
+      //thread t2(t_inet_get_client);
+    thread t0(t_main_c);
+    thread t1(t_move2);
+    thread t3(t_inet_send_client);
 
-      thread t1(t_main);
 
-      char ch;
+    t0.join();
+    t1.join();
+    //t2.join();
+    t3.join();
 
-      bool running;
-      running=true;
-
-      while(running)
-
-      {
-          g->print_game();
-
-          if(_kbhit())
-          {
-              ch=getch();
-
-              if(isEnd==true)
-                  t1.join();
-
-              if(ch=='8')
-
-              {
-                buff[0]='8';
-                  send(sock , buff , strlen(buff) , 0 );
-                  cout<<"gor";
-
-                  g->movePlayer1Up();
-
-              }
-
-              else if(ch=='5')
-
-              {
-                buff[0]='5';
-                  send(sock , buff , strlen(buff) , 0 );
-                  cout<<"dol";
-
-                  g->movePlayer1Down();
-
-              }
-
-          }
-
-            break;
     }
+    case '3':
+      break;
   }
-		case '3':
-			break;
-		}
+
+    exit(0);
 
     return 0;
 
